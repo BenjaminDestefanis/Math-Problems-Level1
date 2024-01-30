21d17ec22d7775cf11013f11410aab705c52e239
benjamindestefanis <benjaminisraelb@gmail.com>
2024-01-29 22:46:19 -0300

    First Exercise

---
+++ Level/level1.js
@@ -0,0 +1,17 @@
+// List of problems
+
+
+
+// 1. Convert Celsius for Faherenhei
+
+const convertCelsiuToFahrenheit = (celsius) => {
+    // La formula para convertir de celsius a Fahrenhei es la temperatura en Celsius
+    // multiplaco por 9/5, mas 32
+    // La funcion recive los celcius (numero positivo), esta debe retornar los celcius 
+    // convertidos a fahrenheit
+    // tu codigo AQUI:
+
+}
+
+
+
