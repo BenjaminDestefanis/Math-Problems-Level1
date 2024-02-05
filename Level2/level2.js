9c34a66fce69b4b86d8ae8f4961d14d13d6d0295
BenjaminDestefanis <benjaminisraelb@gmail.com>
2024-02-05 10:46:56 -0300

    2/5/2024 10:46 AM

--- Level2/level2.js
+++ Level2/level2.js
@@ -8,3 +8,14 @@ const firstFactorial = (num) => {
     // si el numero es 0 o negativo - retornara el mensaje "Numero negativo"
     // tu codigo AQUI:
 }
+
+// 2. Encuentra el resto
+
+const findRemeinder = (num1, num2) => {
+    // La funccion recibe 2 numeros . PRIMERO: devemos averiguar cual es el divisor y el dividendo, 
+    // en este caso el dividendo siempre debe ser el mayor de los 2 numeros. SEGUNDO: la funcion debe 
+    // retornar el resto de la division entre esos numeros. 
+    // en caso de que el divisor sea un 0 retornar el mensaje "No es posible dividr por 0"
+    // tu codigo AQUI:
+}
+
