27db4327d273dc306e77602481c6782dd6261672
BenjaminDestefanis <benjaminisraelb@gmail.com>
2024-02-05 09:35:28 -0300

    2/5/2024 9:35 AM

--- Level2/level2.js
+++ Level2/level2.js
@@ -1,2 +1,10 @@
 // List of problems
 
+// 1. Primer Factorial
+
+const firstFactorial = (num) => {
+    // La funcion recibe un numero, debe retornar el factorial de ese numero
+    // EJ: num = 6 - operacion: 6*5*4*3*2*1 - retornara 720
+    // si el numero es 0 o negativo - retornara el mensaje "Numero negativo"
+    // tu codigo AQUI:
+}
