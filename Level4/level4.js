60cc041c8ce8fd998be39fe8c44f1022424d6ab0
BenjaminDestefanis <benjaminisraelb@gmail.com>
2024-05-09 17:26:50 -0300

    5/9/24 5:26 PM

---
+++ Level4/level4.js
@@ -0,0 +1,7 @@
+// 1. Encontrar primos gemelos 
+
+
+const primosGemelos = (limit) => {
+    // La funcion debe encontrar todos los pares de numeros promos gemelos sin
+    // pasarse del limite  recibio (limit) 
+}
