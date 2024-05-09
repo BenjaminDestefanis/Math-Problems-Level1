e5b038bd117b115f4bb4434224c47f54f5806224
BenjaminDestefanis <benjaminisraelb@gmail.com>
2024-05-09 17:34:12 -0300

    5/9/24 5:34 PM

--- Level4/level4.js
+++ Level4/level4.js
@@ -2,6 +2,16 @@
 
 
 const primosGemelos = (limit) => {
-    // La funcion debe encontrar todos los pares de numeros promos gemelos sin
+    // La funcion debe encontrar todos los pares de numeros primos gemelos sin
     // pasarse del limite  recibio (limit) 
+    // TU CODIGO AQUI:
+    
+    return limit
 }
+
+
+module.exports = {
+    primosGemelos
+}
+
+// 2.
