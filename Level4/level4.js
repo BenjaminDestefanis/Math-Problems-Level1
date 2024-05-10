06bbf335b6d1b654127b3ec7b0f98aadd46fce51
BenjaminDestefanis <benjaminisraelb@gmail.com>
2024-05-10 09:27:56 -0300

    5/10/24 9:27 AM

--- Level4/level4.js
+++ Level4/level4.js
@@ -8,9 +8,22 @@ const primosGemelos = (limit) => {
     return limit
 }
 
+// 2. Finanzas: Calcular el interes compuesto
+
+ const calcularInteresCompuesto = (inversion, tasa, años) => {
+    // La funcion recibe una inversion ( String . Ej: '300 USD' , ' 134,5 $'  etc)
+    // Una tasa anual ( String. Ej: '40 %' , '15 %' etc) y los años.
+    // La funcion debe calcular la tasa de interes compuesto, segun la la inversion,
+    // la tasa y los años y retornarlo en formato string acompañado de la moneda en 
+    // que se inviertio. Ej: " 233 $" 
+    // TU CODIGO AQUI:
+
+ }
+
 
 module.exports = {
-    primosGemelos
+    primosGemelos,
+    calcularInteresCompuesto
 }
 
 // 2. 
